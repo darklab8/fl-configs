92c7cbce82657837bff42efd8348763a62116136
dd84ai <dd84ai@gmail.com>
2024-03-29 17:45:44 +0100

    refactor: delete not used code

--- configs/configs_mapped/freelancer_mapped/data_mapped/missions_mapped/mbases_mapped/mbases.go
+++ configs/configs_mapped/freelancer_mapped/data_mapped/missions_mapped/mbases_mapped/mbases.go
@@ -159,29 +159,6 @@ func Read(input_file *iniload.IniLoader) *Config {
 			}
 		}
 
-		// for _, group_res := range groups {
-		// 	group := &Group{}
-		// 	group.Map(group_res)
-		// 	group.Nickname = semantic.NewString(group_res, "nickname")
-		// 	group.IdsName = semantic.NewInt(group_res, "ids_name")
-		// 	group.IdsInfo = semantic.NewInt(group_res, "ids_info")
-		// 	group.IdsShortName = semantic.NewInt(group_res, "ids_short_name")
-
-		// 	group.Relationships = make([]*Relationship, 0, 20)
-
-		// 	param_rep_key := "rep"
-		// 	for rep_index, _ := range group_res.ParamMap[param_rep_key] {
-
-		// 		rep := &Relationship{}
-		// 		rep.Map(group_res)
-		// 		rep.Rep = semantic.NewFloat(group_res, param_rep_key, semantic.Precision(2), semantic.Index(rep_index))
-		// 		rep.TargetNickname = semantic.NewString(group_res, param_rep_key, semantic.OptsS(semantic.Index(rep_index), semantic.Order(1)))
-		// 		group.Relationships = append(group.Relationships, rep)
-		// 	}
-
-		// 	frelconfig.Groups = append(frelconfig.Groups, group)
-		// 	frelconfig.GroupsMap.MapSet(group.Nickname.Get(), group)
-		// }
 	}
 
 	return frelconfig
