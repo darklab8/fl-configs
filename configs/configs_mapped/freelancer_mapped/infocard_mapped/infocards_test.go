5d5d320fe0b38c088e10c031438046c9a88c9336
dd84ai <dd84ai@gmail.com>
2024-05-14 01:02:52 +0200

    feat: reading infocards overrides

--- configs/configs_mapped/freelancer_mapped/infocard_mapped/infocards_test.go
+++ configs/configs_mapped/freelancer_mapped/infocard_mapped/infocards_test.go
@@ -1,24 +1,28 @@
 package infocard_mapped
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/darklab8/fl-configs/configs/configs_mapped/freelancer_mapped/exe_mapped"
 	"github.com/darklab8/fl-configs/configs/configs_mapped/freelancer_mapped/infocard_mapped/infocard"
 	"github.com/darklab8/fl-configs/configs/configs_mapped/parserutils/filefind"
-
 	"github.com/darklab8/go-utils/goutils/utils"
-
+	"github.com/darklab8/go-utils/goutils/utils/utils_types"
 	"github.com/stretchr/testify/assert"
 )
 
+// Not used any longer?
 func TestReader(t *testing.T) {
-	test_directory := utils.GetCurrrentTestFolder()
+	one_file_filesystem := filefind.FindConfigs(utils.GetCurrrentTestFolder())
+
 	config := infocard.NewConfig()
-	filesystem := filefind.FindConfigs(test_directory)
+	test_directory, _ := filepath.Abs(utils.GetCurrentFolder().ToString())
+	test_directory = filepath.Join(filepath.Dir(test_directory), "exe_mapped", "testdata")
+	filesystem := filefind.FindConfigs(utils_types.FilePath(test_directory))
 
 	freelancer_ini := exe_mapped.FixtureFLINIConfig()
-	config, _ = Read(filesystem, freelancer_ini, filesystem.GetFile(FILENAME))
+	config, _ = Read(filesystem, freelancer_ini, one_file_filesystem.GetFile("temp.disco.infocards.txt"))
 
 	assert.Greater(t, len(config.Infocards), 0)
 }
