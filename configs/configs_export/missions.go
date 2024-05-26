6729b76fe9d4195fc922e764b09f1c1c4dc89cf7
dd84ai <dd84ai@gmail.com>
2024-05-26 22:18:36 +0200

    refactor: to hide non essential code

--- configs/configs_export/missions.go
+++ configs/configs_export/missions.go
@@ -3,12 +3,10 @@ package configs_export
 import (
 	"errors"
 	"fmt"
-	"math"
 	"sort"
 	"strings"
 
 	"github.com/darklab8/fl-configs/configs/configs_mapped/freelancer_mapped/data_mapped/universe_mapped/systems_mapped"
-	"github.com/darklab8/fl-configs/configs/configs_mapped/parserutils/semantic"
 )
 
 type MissioNFaction struct {
@@ -121,6 +119,12 @@ func (e *Exporter) GetMissions(bases []Base, factions []Faction) []Base {
 			continue
 		}
 
+		if base_info.MVendor == nil {
+			base.Missions.Err = errors.New("no mvendor in mbase")
+			bases[base_index] = base
+			continue
+		}
+
 		for _, faction_info := range base_info.BaseFactions {
 			faction := MissioNFaction{
 				FactionNickname: faction_info.Faction.Get(),
@@ -150,17 +154,6 @@ func (e *Exporter) GetMissions(bases []Base, factions []Faction) []Base {
 				base.Missions.Factions = append(base.Missions.Factions, faction)
 				continue
 			}
-
-			// Verify that faction has Spawn zones before adding
-			// Otherwise skip
-			// NEWS: Looks like boolshit condition. Delete once tuned
-			// spawn_zones, found_zones := system.MissionsSpawnZonesByFaction[faction.FactionNickname]
-			// if !found_zones {
-			// 	faction.Err = errors.New("no mission faction npc spawning zones")
-			// 	base.Missions.Factions = append(base.Missions.Factions, faction)
-			// 	continue
-			// }
-			// _ = spawn_zones
 
 			for money_index, diff_to_money := range diffs_to_money {
 
@@ -229,26 +222,9 @@ func (e *Exporter) GetMissions(bases []Base, factions []Faction) []Base {
 
 				// EXPERIMENTAL: Turned off check for vignette check to be within npc spawning zones.
 				// looks to be not necessary
-				// matched_vignette := false
-				// for _, vignette := range system.MissionZoneVignettes {
-
-				// 	distance, dist_err := DistanceForVecs(vignette.Pos, npc_spawn_zone.Pos)
-				// 	if dist_err != nil {
-				// 		continue
-				// 	}
-
-				// 	max_spwn_zone_size, err_max_size := GetMaxRadius(npc_spawn_zone.Size)
-				// 	logus.Log.CheckWarn(err_max_size, "expected finding max size, but object does not have it")
-
-				// 	if distance < float64(vignette.Size.Get())+max_spwn_zone_size {
-				// 		matched_vignette = true
-				// 		break
-				// 	}
-				// }
-
-				// if !matched_vignette {
-				// 	continue
-				// }
+				if !IsAnyVignetteWithinNPCSpawnRange(system, npc_spawn_zone) {
+					continue
+				}
 
 				for _, enemy := range enemies {
 					faction_enemy, faction_found := factions_map[enemy.FactionNickname.Get()]
@@ -328,43 +304,4 @@ func (e *Exporter) GetMissions(bases []Base, factions []Faction) []Base {
 	}
 
 	return bases
-}
-
-func DistanceForVecs(Pos1 *semantic.Vect, Pos2 *semantic.Vect) (float64, error) {
-	if _, ok := Pos1.X.GetValue(); !ok {
-		return 0, errors.New("no x")
-	}
-	if _, ok := Pos2.X.GetValue(); !ok {
-		return 0, errors.New("no x")
-	}
-
-	x_dist := math.Pow((Pos1.X.Get() - Pos2.X.Get()), 2)
-	y_dist := math.Pow((Pos1.Y.Get() - Pos2.Y.Get()), 2)
-	z_dist := math.Pow((Pos1.Z.Get() - Pos2.Z.Get()), 2)
-	distance := math.Pow((x_dist + y_dist + z_dist), 0.5)
-	return distance, nil
-}
-
-func GetMaxRadius(Size *semantic.Vect) (float64, error) {
-	max_size := 0.0
-	if value, ok := Size.X.GetValue(); ok {
-		if value > max_size {
-			max_size = value
-		}
-	}
-	if value, ok := Size.Y.GetValue(); ok {
-		if value > max_size {
-			max_size = value
-		}
-	}
-	if value, ok := Size.Z.GetValue(); ok {
-		if value > max_size {
-			max_size = value
-		}
-	}
-	if max_size == 0 {
-		return 0, errors.New("not found size")
-	}
-
-	return max_size, nil
 }
