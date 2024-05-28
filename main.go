b5edf967872342983bc0616c64921fe2d229e80c
dd84ai <dd84ai@gmail.com>
2024-05-28 12:42:37 +0200

    fix: uniform FreelancerFolder variable

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/darklab8/fl-configs/configs/configs_export"
 	"github.com/darklab8/fl-configs/configs/configs_mapped"
-	"github.com/darklab8/fl-configs/configs/configs_mapped/configs_fixtures"
+	"github.com/darklab8/fl-configs/configs/settings"
 	"github.com/darklab8/fl-configs/configs/settings/logus"
 	"github.com/darklab8/go-utils/goutils/utils/time_measure"
 	"github.com/darklab8/go-utils/goutils/utils/utils_logus"
@@ -30,7 +30,7 @@ func main() {
 		time_measure.TimeMeasure(func(m *time_measure.TimeMeasurer) {
 			var configs *configs_mapped.MappedConfigs
 			time_measure.TimeMeasure(func(m *time_measure.TimeMeasurer) {
-				freelancer_folder := configs_fixtures.FixtureGameLocation()
+				freelancer_folder := settings.GetGameLocation()
 
 				configs = configs_mapped.NewMappedConfigs()
 				logus.Log.Debug("scanning freelancer folder", utils_logus.FilePath(freelancer_folder))
