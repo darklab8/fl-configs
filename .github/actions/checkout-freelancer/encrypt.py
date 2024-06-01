23c0fe6fcc0fad2f6443bc153645624b11dbd6ce
dd84ai <dd84ai@gmail.com>
2024-06-01 02:44:53 +0200

    ci: add debugging bits

--- .github/actions/checkout-freelancer/encrypt.py
+++ .github/actions/checkout-freelancer/encrypt.py
@@ -1,13 +1,15 @@
 import base64
 import os
 
-with open("~/.ssh/id_rsa.files.freelancer.vanilla", "rb") as file:
+with open("ssh_key", "rb") as file:
     data = file.read()
 
 env_var = str(base64.b64encode(data),encoding='utf8')
 os.environ["ID_RSA_FILES_FREELANCER_VANILLA"] =env_var
+
 print(env_var)
 print("\n\n")
 
-with open("~/.ssh/id_rsa.files.freelancer.vanilla.out", "wb", 0o600) as file:
+with open("ssh_key.out", "wb", 0o600) as file:
     file.write(base64.b64decode(bytes(os.environ["ID_RSA_FILES_FREELANCER_VANILLA"], encoding='utf8')))
+os.chmod("ssh_key.out", 0o600)
