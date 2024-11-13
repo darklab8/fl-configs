62271abafff16122cafefaec40fb1f5b4ac1f279
dd84ai <dd84ai@gmail.com>
2024-11-13 07:15:29 +0100

    fix: drydocks are always visible

--- configs/configs_export/bases.go
+++ configs/configs_export/bases.go
@@ -130,6 +130,9 @@ func FilterToUserfulBases(bases []*Base) []*Base {
 				is_invisible = false
 			}
 		}
+		if strings.Contains(item.Name, "Drydock") {
+			is_invisible = false
+		}
 		if is_invisible {
 			continue
 		}
