1d9a7ae5b27967ec78185abc2a43d2ef29a5f443
dd84ai <dd84ai@gmail.com>
2024-11-17 04:16:54 +0100

    refactor: flhash to be in same package

--- configs/configs_mapped/freelancer_mapped/data_mapped/initialworld/flhash/flhash_faction.go
+++ configs/configs_mapped/freelancer_mapped/data_mapped/initialworld/flhash/flhash_faction.go
@@ -1,8 +1,6 @@
-package factionhash
+package flhash
 
 import (
-	"fmt"
-	"strconv"
 	"strings"
 )
 
@@ -10,13 +8,13 @@ const (
 	FLFACTIONHASH_POLYNOMIAL = 0x1021
 )
 
-type hasher struct {
+type faction_hasher struct {
 	table [256]uint32
 }
 
 // Function for calculating the Freelancer data nickname hash.
 // Algorithm from flhash.exe by sherlog@t-online.de (2003-06-11)
-func (h *hasher) RawHash(data []byte) uint32 {
+func (h *faction_hasher) RawHash(data []byte) uint32 {
 	var hash uint32 = 0xFFFF
 	for _, b := range data {
 		hash = (hash >> 8) ^ h.table[byte(hash&0xFF)^b]
@@ -25,44 +23,32 @@ func (h *hasher) RawHash(data []byte) uint32 {
 }
 
 // NicknameHasher implements the hashing algorithm used by item, base, etc. nicknames
-type NicknameHasher struct {
-	hasher
+type FactionNickHasher struct {
+	faction_hasher
 }
 
-type HashCode int
-
-func (h HashCode) ToIntStr() string {
-	return fmt.Sprintf("%d", int32(h))
-}
-
-func (h HashCode) ToUintStr() string {
-	return strconv.FormatUint(uint64(int(h)), 10)
-}
-
-func (h HashCode) ToHexStr() string {
-	return fmt.Sprintf("%x", int(h))
-}
-
-func (h *NicknameHasher) Hash(name string) HashCode {
+func (h *FactionNickHasher) Hash(name string) HashCode {
 	bytes := []byte(strings.ToLower(name))
 	hash := h.RawHash(bytes)
 	// hash = (hash >> (physicalBits - logicalBits)) | 0x80000000
 	return HashCode(hash)
 }
 
-func NewHasher() *NicknameHasher {
-	h := NicknameHasher{}
+func NewFactionHasher() *FactionNickHasher {
+	h := FactionNickHasher{}
 	NotSimplePopulateTable(FLFACTIONHASH_POLYNOMIAL, &h.table)
-
 	return &h
 }
 
-var nick = NewHasher()
+var faction = NewFactionHasher()
 
 func HashFaction(name string) HashCode {
-	return nick.Hash(name)
+	return faction.Hash(name)
 }
 
+// Copied foundation from golang crc32 that had SimplePopulate
+// And then modified to how it should at Python version given by Alex
+// Matches also Laz version in C# i think
 func NotSimplePopulateTable(poly uint32, t *[256]uint32) {
 	for i := 0; i < 256; i++ {
 		crc := uint32(i) << 8
--- configs/configs_mapped/freelancer_mapped/data_mapped/initialworld/flhash/flhash_faction_test.go
+++ configs/configs_mapped/freelancer_mapped/data_mapped/initialworld/flhash/flhash_faction_test.go
@@ -1,4 +1,4 @@
-package factionhash
+package flhash
 
 import (
 	"fmt"
