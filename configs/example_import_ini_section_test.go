b2bfa24557791772e550cd2b06ae6489f1cf337e
dd84ai <dd84ai@gmail.com>
2024-11-21 13:32:25 +0100

    docs: how to add raw data imported

--- configs/example_import_ini_section_test.go
+++ configs/example_import_ini_section_test.go
@@ -1,9 +1,13 @@
 package configs
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/darklab8/fl-configs/configs/configs_mapped"
 	"github.com/darklab8/fl-configs/configs/configs_mapped/freelancer_mapped/data_mapped/equipment_mapped/equip_mapped"
-	"github.com/darklab8/fl-configs/configs/configs_mapped/parserutils/inireader"
+	"github.com/darklab8/fl-configs/configs/configs_mapped/parserutils/filefind/file"
+	"github.com/darklab8/fl-configs/configs/configs_mapped/parserutils/iniload"
 	"github.com/darklab8/fl-configs/configs/configs_settings"
 	"github.com/darklab8/fl-configs/configs/configs_settings/logus"
 	"github.com/darklab8/go-utils/utils/utils_logus"
@@ -21,16 +25,38 @@ func Example_importIniSection() {
 	// which have both reading and writing back capabilities
 	configs.Read(freelancer_folder)
 
-	var new_section *inireader.Section = &inireader.Section{}
-	mapped_gun := &equip_mapped.Gun{}
-	mapped_gun.Map(new_section)
+	// TODO Adding Section manually
+	// var new_section *inireader.Section = &inireader.Section{}
+	// mapped_gun := &equip_mapped.Gun{}
+	// mapped_gun.Map(new_section)
 
-	mapped_gun.Nickname.Set("my_gun_nickname")
-	mapped_gun.IdsName.Set(3453453)
-	mapped_gun.HPGunType.Set("some_hp_type")
+	// mapped_gun.Nickname.Set("my_gun_nickname")
+	// mapped_gun.IdsName.Set(3453453)
+	// mapped_gun.HPGunType.Set("some_hp_type")
 
-	configs.Equip.Files[0].Sections = append(configs.Equip.Files[0].Sections, new_section)
+	// configs.Equip.Files[0].Sections = append(configs.Equip.Files[0].Sections, new_section)
+
+	///////////// Alternatively feeding in memory section entirely
+
+	content := `
+[Gun]
+nickname = some_gun
+hp_gun_type = some_hp_type
+`
+	memory_file := file.NewMemoryFile(strings.Split(content, "\n"))
+	scanned_file := iniload.NewLoader(memory_file).Scan()
+
+	mapped_equip := equip_mapped.Read([]*iniload.IniLoader{scanned_file})
+	fmt.Println(mapped_equip.Guns[0].Nickname.Get())
+	fmt.Println(mapped_equip.Guns[0].HPGunType.Get())
+
+	// Adding to main Freelancer instance..
+	configs.Equip.Files[0].Sections = append(configs.Equip.Files[0].Sections, mapped_equip.Guns[0].Model.RenderModel())
 
 	// Write without Dry Run for writing to files modified values back!
 	configs.Write(configs_mapped.IsDruRun(true))
+
+	// Output:
+	// some_gun
+	// some_hp_type
 }
